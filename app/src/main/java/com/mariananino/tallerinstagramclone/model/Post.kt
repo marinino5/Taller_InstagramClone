bb1757576e4c905a7d5532add3d5f83814c0504c
Mariana Niño Solano <mnino499@unab.edu.co>
2026-03-13 11:05:05 -0500

    feat: add Post and Story models

--- app/src/main/java/com/mariananino/tallerinstagramclone/model/Post.kt
+++ app/src/main/java/com/mariananino/tallerinstagramclone/model/Post.kt
@@ -1,2 +1,11 @@
 package com.mariananino.tallerinstagramclone.model
 
+data class Post(
+    val id: Int,
+    val username: String,
+    val profileImageUrl: String,
+    val imageUrl: String,
+    val likes: Int,
+    val caption: String,
+    val isLiked: Boolean = false
+)
--- app/src/main/java/com/mariananino/tallerinstagramclone/model/Story.kt
+++ app/src/main/java/com/mariananino/tallerinstagramclone/model/Story.kt
@@ -1,2 +1,8 @@
 package com.mariananino.tallerinstagramclone.model
 
+data class Story(
+    val id: Int,
+    val username: String,
+    val profileImageUrl: String,
+    val isSeen: Boolean = false
+)
